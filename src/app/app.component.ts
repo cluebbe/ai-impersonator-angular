f010ff7b03db2aecb8a736af1117205272a14360
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-04-25 13:06:52 +0200

    configured routing and added contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ChatComponent } from './chat/chat.component';
 import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, ChatComponent, HeaderComponent],
+  imports: [RouterModule,  HeaderComponent],
 })
 
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: ChatComponent},
-    {path: 'home', component: ChatComponent}
+    {path: 'home', component: ChatComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', component: ChatComponent}   
 ];
