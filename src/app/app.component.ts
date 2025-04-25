1d6fb1e3c37f5c2cc86e95d283a426de055e4ea6
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-04-25 10:59:38 +0200

    included bootstrap. Generated header stub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, ChatComponent],
+  imports: [RouterModule, ChatComponent, HeaderComponent],
 })
 
 export class AppComponent {
