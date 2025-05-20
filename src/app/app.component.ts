4bc21be55f4979817ab848bfb122c9a04d4b307d
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-05-20 12:37:02 +0200

    created module and base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NotificationModule } from './notification/notification.module';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule,  HeaderComponent],
+  imports: [RouterModule, HeaderComponent, NotificationModule],
 })
 
 export class AppComponent {
