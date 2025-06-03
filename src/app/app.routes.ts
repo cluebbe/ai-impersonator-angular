54de5e44953c01fc16d8de1fc68ca40c36a386b5
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-06-03 10:47:07 +0200

    created dashboard component and authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: '', component: ChatComponent},
     {path: 'home', component: ChatComponent},
     {path: 'contact', component: ContactComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
     {path: '**', component: ChatComponent}   
 ];
